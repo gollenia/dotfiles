3cb0b0e7846f15818ca41d45d2e66151bfc58c98
Thomas Gollenia <thomas@gollenia.at>
2021-02-25 20:12:47 +0100

    .zshrc added

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .tmux.conf ~/
 cp .vimrc ~/
 cp .profile ~/
